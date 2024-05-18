c2c07f0b50acdc099c67419b08836962df4b4593
sasha1985y <kuyancev1985@mail.ru>
2024-05-18 03:44:43 +0300

    4.13. Добавит уникальность для поля email.WIP: Начальное состояние. Модель не используется по назначению.

--- src/shared/modules/user/user.model.ts
+++ src/shared/modules/user/user.model.ts
@@ -8,11 +8,13 @@ export interface UserDocument extends User, Document {
 }
 
 const userSchema = new Schema({
-  email: String,
+  email: {
+    type: String,
+    unique: true,
+  },
   avatarPath: String,
   firstname: String,
   lastname: String,
-
 }, { timestamps: true });
 
 export const UserModel = model<UserDocument>('User', userSchema);
