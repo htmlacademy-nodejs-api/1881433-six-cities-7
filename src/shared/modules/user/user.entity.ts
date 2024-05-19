974804e5a451c095bde677e7c25415579d026969
sasha1985y <kuyancev1985@mail.ru>
2024-05-19 21:15:22 +0300

    4.20. Добавит методы в UserEntity.

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,24 +1,53 @@
-import { defaultClasses, getModelForClass, prop } from '@typegoose/typegoose';
+import { defaultClasses, getModelForClass, prop, modelOptions } from '@typegoose/typegoose';
 
 import { User } from '../../types/index.js';
+import { createSHA256 } from '../../helpers/index.js';
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export interface UserEntity extends defaultClasses.Base {}
 
+@modelOptions({
+  schemaOptions: {
+    collection: 'users',
+    timestamps: true,
+  }
+})
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export class UserEntity extends defaultClasses.TimeStamps implements User {
-  password: string;
-  typeUser: string;
+
   @prop({ unique: true, required: true })
   public email: string;
 
   @prop({ required: false, default: '' })
   public avatarPath: string;
 
-  @prop({ required: true })
+  @prop({ required: true, default: '' })
   public firstname: string;
 
-  @prop({ required: true })
+  @prop({ required: true, default: '' })
   public lastname: string;
+
+  @prop({ required: true, default: '' })
+  private password?: string;
+
+  @prop({ required: false, default: '' })
+  public typeUser: string;
+
+  constructor(userData: User) {
+    super();
+
+    this.email = userData.email;
+    this.avatarPath = userData.avatarPath;
+    this.firstname = userData.firstname;
+  }
+
+  public setPassword(password: string, salt: string) {
+    this.password = createSHA256(password, salt);
+  }
+
+  public getPassword() {
+    return this.password;
+  }
 }
+
 export const UserModel = getModelForClass(UserEntity);
