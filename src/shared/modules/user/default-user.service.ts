cf92bbce9ebbd4a67f054641d9a7d2df07801602
sasha1985y <kuyancev1985@mail.ru>
2024-05-20 21:12:44 +0300

    4.27. Добавит имплементацию методов UserService.

--- src/shared/modules/user/default-user.service.ts
+++ src/shared/modules/user/default-user.service.ts
@@ -1,4 +1,4 @@
-import { DocumentType } from '@typegoose/typegoose';
+import { DocumentType, types } from '@typegoose/typegoose';
 import { inject, injectable } from 'inversify';
 
 import { UserService } from './user-service.interface.js';
@@ -16,10 +16,23 @@ export class DefaultUserService implements UserService {
   public async create(dto: CreateUserDto, salt: string): Promise<DocumentType<UserEntity>> {
     const user = new UserEntity(dto);
     user.setPassword(dto.password, salt);
-
     const result = await this.userModel.create(user);
     this.logger.info(`New user created: ${user.email}`);
 
     return result;
+  }
+
+  public async findByEmail(email: string): Promise<DocumentType<UserEntity> | null> {
+    return this.userModel.findOne({email});
+  }
+
+  public async findOrCreate(dto: CreateUserDto, salt: string): Promise<DocumentType<UserEntity>> {
+    const existedUser = await this.findByEmail(dto.email);
+
+    if (existedUser) {
+      return existedUser;
+    }
+
+    return this.create(dto, salt);
   }
 }
