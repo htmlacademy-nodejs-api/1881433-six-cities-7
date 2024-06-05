d68e41d9e7123adeea6c399cf291e24d359f0275
sasha1985y <kuyancev1985@mail.ru>
2024-06-06 00:30:05 +0300

    5.8. Добавит выборку из связанных коллекций в DefaultOfferService.

--- src/shared/modules/offer/default-offer.service.ts
+++ src/shared/modules/offer/default-offer.service.ts
@@ -1,12 +1,10 @@
 import { inject, injectable } from 'inversify';
 import { DocumentType, types } from '@typegoose/typegoose';
-
 import { OfferService } from './offer-service.interface.js';
 import { Component } from '../../types/index.js';
 import { Logger } from '../../libs/logger/index.js';
 import { OfferEntity } from './offer.entity.js';
 import { CreateOfferDto } from './dto/create-offer.dto.js';
-
 @injectable()
 export class DefaultOfferService implements OfferService {
   constructor(
@@ -17,11 +15,13 @@ export class DefaultOfferService implements OfferService {
   public async create(dto: CreateOfferDto): Promise<DocumentType<OfferEntity>> {
     const result = await this.offerModel.create(dto);
     this.logger.info(`New offer created: ${dto.title}`);
-
     return result;
   }
 
   public async findById(offerId: string): Promise<DocumentType<OfferEntity> | null> {
-    return this.offerModel.findById(offerId).exec();
+    return this.offerModel
+      .findById(offerId)
+      .populate(['userId', 'categories'])
+      .exec();
   }
 }
