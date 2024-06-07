309d4266337557f1da61673df47849005ba578de
sasha1985y <kuyancev1985@mail.ru>
2024-06-07 17:20:40 +0300

    5.11. Добавит отрефакторенный CommentEntity.

--- src/shared/modules/comments/comment.entity.ts
+++ src/shared/modules/comments/comment.entity.ts
@@ -6,35 +6,33 @@ import {
   Ref
 } from '@typegoose/typegoose';
 
+import { OfferEntity } from '../offer/index.js';
 import { UserEntity } from '../user/index.js';
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export interface CommentEntity extends defaultClasses.Base {}
 
-    @modelOptions({
-      schemaOptions: {
-        collection: 'comments',
-        timestamps: true,
-      }
-    })
-
+@modelOptions({
+  schemaOptions: {
+    collection: 'comments'
+  }
+})
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export class CommentEntity extends defaultClasses.TimeStamps {
-    @prop({ trim: true, required: true })
+  @prop({ trim: true, required: true })
   public text: string;
 
-    @prop({ required: true })
-    public postDate: Date;
+  @prop({
+    ref: OfferEntity,
+    required: true
+  })
+  public offerId: Ref<OfferEntity>;
 
-    @prop({ required: true })
-    public rating: number;
-
-    @prop({
-      ref: UserEntity,
-      required: true
-    })
-    public userId: Ref<UserEntity>;
-
+  @prop({
+    ref: UserEntity,
+    required: true,
+  })
+  public userId: Ref<UserEntity>;
 }
 
 export const CommentModel = getModelForClass(CommentEntity);
